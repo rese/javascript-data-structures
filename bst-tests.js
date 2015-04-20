38b433f922fe3b51a9e33a5213e8ded7eb635ec2
Rese Seares <tseares@zappos.com>
2015-04-20 16:15:34 -0700

    Add test to contains function

--- bst-tests.js
+++ bst-tests.js
@@ -13,3 +13,9 @@ bst.add(7);
 bst.add(13);
 
 console.log(bst); 
+
+// Test `contains` function
+console.log('contains 8?', bst.contains(8));   	//true
+console.log('contains 1?', bst.contains(1));  	//true
+console.log('contains 14?', bst.contains(14));  //true
+console.log('contains 11?', bst.contains(11));	//false
