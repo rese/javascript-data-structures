924b975607c996b83c7c9ef4fb8a25fb5eba9946
Rese Seares <tseares@zappos.com>
2015-04-20 16:14:13 -0700

    Create bst-tests.js to hold all testing operations for BST.

---
+++ bst-tests.js
@@ -0,0 +1,15 @@
+// Create a binary search tree
+var bst = new BinarySearchTree();
+
+// Test `add` function
+bst.add(8);
+bst.add(3);
+bst.add(10);
+bst.add(1);
+bst.add(6);
+bst.add(14);
+bst.add(4);
+bst.add(7);
+bst.add(13);
+
+console.log(bst); 
